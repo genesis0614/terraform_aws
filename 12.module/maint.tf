f849554ad3187a5178441bd5cbc56b636d63b614
genesis0614 <82128297+genesis0614@users.noreply.github.com>
2024-11-20 14:47:19 +0900

    Update maint.tf

--- 12.module/maint.tf
+++ 12.module/maint.tf
@@ -16,13 +16,13 @@ data "aws_availability_zones" "available" {
 }
 
 module "vpc" {
-  source         = "./vpc"
+  source         = "https://github.com/genesis0614/terraform_aws/12.module/vpc"
   pjt_name       = "netid"
   vpc_cidr_block = "10.0.0.0/16"
 }
 
 module "pub_sn_1" {
-  source        = "./subnet"
+  source        = "https://github.com/genesis0614/terraform_aws/12.module/subnet"
   pjt_name      = var.pjt_name
   vpc_id        = module.vpc.vpc_id
   sn_cidr_block = "10.0.1.0/24"
@@ -30,7 +30,7 @@ module "pub_sn_1" {
 }
 
 module "pub_sn_2" {
-  source        = "./subnet"
+  source        = "https://github.com/genesis0614/terraform_aws/12.module/subnet"
   pjt_name      = var.pjt_name
   vpc_id        = module.vpc.vpc_id
   sn_cidr_block = "10.0.2.0/24"
